3a1c98acc6e4e137c4bfc0ce02ff9ceb806cc3d5
suganthsuji <suganthpv.007@gmail.com>
2022-07-18 19:30:32 +0530

    Added PartTime

--- src/main/java/com/example/Employee.java
+++ src/main/java/com/example/Employee.java
@@ -1,11 +1,27 @@
 package com.example;
 
+import java.util.Scanner;
+
 public class Employee extends EmployeeAttendance {
     public static void main(String[] args) {
         System.out.println("Welcome to EmployeeWage Problem");
         EmployeeAttendance employeeAttendance=new EmployeeAttendance();
-        int attendance=employeeAttendance.employeeAttendance();
 
-        employeeAttendance.calculateWage(attendance);
+
+
+        Scanner scanner=new Scanner(System.in);
+        int time=scanner.nextInt();
+        switch (time){
+            case 0:
+                System.out.println("Part Time");
+                int attendance=employeeAttendance.employeeAttendance();
+                employeeAttendance.calculateWage(attendance,0);
+                break;
+            case 1:
+                System.out.println("Full Time");
+                 attendance=employeeAttendance.employeeAttendance();
+                employeeAttendance.calculateWage(attendance,1);
+                break;
+        }
     }
 }
