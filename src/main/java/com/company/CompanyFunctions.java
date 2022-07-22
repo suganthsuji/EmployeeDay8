abdce5d838c304a14f9a478da8d2792773687ad2
suganthsuji <suganthpv.007@gmail.com>
2022-07-22 19:07:49 +0530

    Added: uc7-uc14

---
+++ src/main/java/com/company/CompanyFunctions.java
@@ -0,0 +1,14 @@
+package com.company;
+
+/*
+Company Interface that has the function of a Company
+ */
+public interface CompanyFunctions
+{
+    void addCompany();
+    void showAllCompany();
+    void showCompany(String companyName);
+    void deleteCompany(String companyName);
+    void getTotalWage(String companyName);
+    void employeeDetails(String companyName);
+}
